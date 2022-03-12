3f408152a22905e664a15cfa0b4964b9f2aeb52c
C8linR <45794604+c8linr@users.noreply.github.com>
2022-03-12 14:37:35 -0500

    Implement the location input

--- CropProfitabilityCalculator/server.R
+++ CropProfitabilityCalculator/server.R
@@ -14,42 +14,36 @@ shinyServer(function(input, output, session) {
   #Create the list of crops for the user to choose from
   crop_vector <- c(dbGetQuery(con, "SELECT crop FROM cropdata.crop_list;"))
   
+  #Disconnect from the database
+  dbDisconnect(con)
+  
   #Update the crop selection widget
   updateSelectInput(session, "crop", choices = crop_vector)
   
-  #Get the list of valid locations
-  canada_places <- c(dbGetQuery(con, "SELECT DISTINCT PNname FROM cropdata.place_names;"))
-  
-  #Validate the location
+  #Validate the location when the submit button is pressed
   output$calculation <- eventReactive(input$calculate, {
-      valid_location <- FALSE
-      for(place in canada_places) {
-        compare_places <- identical(tolower(input$location), tolower(place))
-        if(compare_places) {
-          valid_location = TRUE
-        }
-      }
-      shinyFeedback::feedbackWarning("location", !valid_location, "Invalid location")
-      req(valid_location)
-      calculation <- str_c("The estimated profitability for ", input$crop, " in ", str_to_title(input$location), " is $X")
-    })
-  
-  #Update the output
-  
-  
-  # output$calculation <- reactive({
-  #   #Validate the location
-  #   is_valid_location <- FALSE
-  #   for(place in canada_places) {
-  #     compare_places <- identical(tolower(input$location), tolower(place))
-  #     if(compare_places) {
-  #       is_valid_location = TRUE
-  #     }
-  #   }
-  #   shinyFeedback::feedbackWarning("location", !is_valid_location, "Invalid location")
-  #   calculation <- str_c("The estimated profitability for ", input$crop, " in ", input$location, " is $X")
-  # })
-  
-  #Disconnect from the database
-  dbDisconnect(con)
+    # Connect to the database IN SCOPE
+    con2 <- dbConnect(RMariaDB::MariaDB(), default.file='..\\.my.cnf', group='rs-dbi', dbname='cropdata')
+    
+    # Construct the query string
+    query <- str_c("SELECT DISTINCT PNname FROM cropdata.place_names WHERE PNname=\"", str_to_lower(input$location), "\";")
+    
+    #Get the result set of the query
+    res <- c(dbGetQuery(con2, query))
+    
+    #Determine if the location is valid
+    valid_location <- identical(c(str_to_lower(input$location)), c(tolower(res)))
+    
+    #Display a warning if the location is invalid
+    shinyFeedback::feedbackWarning("location", !valid_location, "Invalid location")
+    
+    #Require the location to be valid
+    req(valid_location)
+    
+    #Close the in-scope DB connection
+    dbDisconnect(con2)
+    
+    #Display the result
+    calculation <- str_c("The estimated profitability for ", input$crop, " in ", str_to_title(input$location), " is $X")
+  })
 })
